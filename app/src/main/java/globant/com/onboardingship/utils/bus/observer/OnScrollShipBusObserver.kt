040fddd4646e19ddf5ddff799238543222d6e776
GLOBANT\taidy.marrugo <taidy.marrugo@globant.com>
2019-01-07 11:11:32 -0500

    bus observer

---
+++ app/src/main/java/globant/com/onboardingship/utils/bus/observer/OnScrollShipBusObserver.kt
@@ -0,0 +1,7 @@
+package globant.com.onboardingship.utils.bus.observer
+
+abstract class OnScrollShipBusObserver : BusObserver<OnScrollShipBusObserver.OnScrollMoved>
+(OnScrollMoved::class.java) {
+
+    class OnScrollMoved
+}
